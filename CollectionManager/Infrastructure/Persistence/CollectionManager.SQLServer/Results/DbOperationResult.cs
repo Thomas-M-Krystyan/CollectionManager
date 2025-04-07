d9735537163886b2f1a7152dbb0ac533603601b3
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-08 00:58:00 +0200

    Add DbOperationResult

---
+++ CollectionManager/Infrastructure/Persistence/CollectionManager.SQLServer/Results/DbOperationResult.cs
@@ -0,0 +1,33 @@
+﻿namespace CollectionManager.SQLServer.Results
+{
+    /// <summary>
+    /// The standardized database operation result.
+    /// </summary>
+    public readonly struct DbOperationResult(bool isSuccess, int changesCount, string message)
+    {
+        /// <summary>
+        /// Indicator whether the database operation was successful.
+        /// </summary>
+        public bool IsSuccess { get; } = isSuccess;
+
+        /// <summary>
+        /// The number of affected database rows.
+        /// </summary>
+        public int ChangesCount { get; } = changesCount;
+
+        /// <summary>
+        /// The database operation result message.
+        /// </summary>
+        public string Message { get; } = message;
+
+        /// <summary>
+        /// The operation was successful.
+        /// </summary>
+        internal static DbOperationResult Success(int changesCount, string message) => new(true, changesCount, message);
+
+        /// <summary>
+        /// The operation was unsuccessful.
+        /// </summary>
+        internal static DbOperationResult Failure(string message) => new(false, 0, message);
+    }
+}
