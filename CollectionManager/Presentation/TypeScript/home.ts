b107f5168e5f1e30ba54ba138e0612d45ac41915
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-07 23:21:00 +0200

    Cleanup

--- CollectionManager/Presentation/TypeScript/home.ts
+++ CollectionManager/Presentation/TypeScript/home.ts
@@ -1,2 +1,1 @@
-﻿let titleElement: HTMLHeadingElement = document.querySelector("h1");
-titleElement.innerHTML = "Hello from TypeScript";
+﻿
--- CollectionManager/Presentation/wwwroot/js/home.js
+++ CollectionManager/Presentation/wwwroot/js/home.js
@@ -1,3 +1,1 @@
-var titleElement = document.querySelector("h1");
-titleElement.innerHTML = "Hello from TypeScript";
 //# sourceMappingURL=home.js.map
