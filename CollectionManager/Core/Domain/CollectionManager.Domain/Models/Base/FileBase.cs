92f3f27c72f95013271e88432e08697c0e728509
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-08 03:19:23 +0200

    Update FileBase.cs

--- CollectionManager/Core/Domain/CollectionManager.Domain/Models/Base/FileBase.cs
+++ CollectionManager/Core/Domain/CollectionManager.Domain/Models/Base/FileBase.cs
@@ -1,7 +1,7 @@
 ﻿namespace CollectionManager.Domain.Models.Base
 {
     /// <summary>
-    /// The image of an item.
+    /// The file.
     /// </summary>
     public record FileBase<TExtension>
         where TExtension : Enum
@@ -9,22 +9,22 @@
         /// <summary>
         /// The unique identifier of the file.
         /// </summary>
-        public required int Id { get; set; }
+        public required int Id { get; init; }
 
         /// <summary>
         /// The name of the file.
         /// </summary>
-        public required string Name { get; set; }
+        public required string Name { get; init; }
 
         /// <summary>
         /// The extension of the file.
         /// </summary>
-        public required TExtension Extension { get; set; }
+        public required TExtension Extension { get; init; }
 
         /// <summary>
         /// The bytes representing the file.
         /// </summary>
-        public required byte[] Bytes { get; set; }
+        public required byte[] Bytes { get; init; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FileBase{TExtension}"/> class.
