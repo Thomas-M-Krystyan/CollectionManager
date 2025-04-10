1ea7f0897a1b1df38bd6d743f00cfc5e267bd567
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-11 00:43:30 +0200

    Add DTO models

---
+++ CollectionManager/Core/Application/CollectionManager.Logic/Models/DTOs/FileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionManager.Logic.Models.DTOs
+{
+    public readonly struct FileDto<TExtension>
+        where TExtension : Enum
+    {
+        public string Name { get; init; }
+
+        public TExtension Extension { get; init; }
+
+        public byte[] Bytes { get; init; }
+    }
+}
