09eddf8dfb01fa13912ef086571f2ce3e5c2c7c5
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-12 00:03:50 +0200

    Genres enum

---
+++ CollectionManager/Core/Domain/CollectionManager.Domain/Enums/Genres/Literatures.cs
@@ -0,0 +1,121 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollectionManager.Domain.Enums.Genres
+{
+    /// <summary>
+    /// Supported item ategories.
+    /// </summary>
+    public enum Literatures
+    {
+        Action,
+
+        Adventure,
+
+        Comedy,
+
+        Crime,
+
+        Cyberpunk,
+
+        Detective,
+
+        Documentary,
+
+        Drama,
+
+        Dystopian,
+
+        Erotic,
+
+        Family,
+
+        Fantasy,
+
+        Fiction,
+
+        Historical,
+
+        Horror,
+
+        Manga,
+
+        Mystery,
+
+        Mythology,
+
+        [Display(Name = "Non-Fiction")]
+        NonFiction,
+
+        Parody,
+
+        Philosophical,
+
+        Political,
+
+        Pornographic,
+
+        [Display(Name = "Post-Apocalyptic")]
+        PostApocalyptic,
+
+        Psychological,
+
+        Romance,
+
+        Satire,
+
+        Science,
+
+        [Display(Name = "Science-Fiction")]
+        ScienceFiction,
+
+        [Display(Name = "Slice of Life")]
+        SliceOfLife,
+
+        Space,
+
+        [Display(Name = "Space Opera")]
+        SpaceOpera,
+
+        Sports,
+
+        [Display(Name = "Steampunk")]
+        Steampunk,
+
+        [Display(Name = "Super Hero")]
+        Superhero,
+
+        [Display(Name = "Super Heroine")]
+        Superheroine,
+
+        Supernatural,
+
+        [Display(Name = "Super Villain")]
+        Supervillain,
+
+        Suspense,
+
+        [Display(Name = "Sword & Sorcery")]
+        SwordAndSorcery,
+
+        Technology,
+
+        Thriller,
+
+        [Display(Name = "Time Travel")]
+        TimeTravel,
+
+        Tragedy,
+
+        Urban,
+
+        Utopian,
+
+        Vampire,
+
+        War,
+
+        Western,
+
+        Other
+    }
+}
