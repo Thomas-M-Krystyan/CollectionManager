0b413fa71a30a1116e791bf719708d56f4e90c32
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-12 00:33:58 +0200

    Add Comic domain model

---
+++ CollectionManager/Core/Domain/CollectionManager.Domain/Models/Collectibles/Comic.cs
@@ -0,0 +1,30 @@
+﻿using CollectionManager.Domain.Enums.Categories;
+using CollectionManager.Domain.Enums.Genres;
+using CollectionManager.Domain.Models.Base;
+
+namespace CollectionManager.Domain.Models.Collectibles
+{
+    /// <summary>
+    /// <inheritdoc cref="ItemBase"/>
+    /// <para>
+    ///   Represents a comic book or graphic novel.
+    /// </para>
+    /// </summary>
+    public sealed record Comic : ItemBase
+    {
+        public required string Authors { get; init; }
+
+        public required Literatures Genre { get; init; }
+
+        public required Ages Age { get; init; }
+
+        public required DateOnly Published { get; init; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Comic"/> class.
+        /// </summary>
+        public Comic() : base()
+        {
+        }
+    }
+}
