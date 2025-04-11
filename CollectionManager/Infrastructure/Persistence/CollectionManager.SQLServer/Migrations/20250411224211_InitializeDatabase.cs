1510deccac5bbf00e87b8d7667384fecd44a68e5
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-12 00:45:04 +0200

    Add new migrations

---
+++ CollectionManager/Infrastructure/Persistence/CollectionManager.SQLServer/Migrations/20250411224211_InitializeDatabase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollectionManager.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Extension = table.Column<byte>(type: "tinyint", nullable: false),
+                    Bytes = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comics",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Authors = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Genre = table.Column<byte>(type: "tinyint", nullable: false),
+                    Age = table.Column<byte>(type: "tinyint", nullable: false),
+                    Published = table.Column<DateOnly>(type: "date", nullable: false),
+                    IsOwned = table.Column<bool>(type: "bit", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ImageId = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comics_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comics_ImageId",
+                table: "Comics",
+                column: "ImageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comics");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
