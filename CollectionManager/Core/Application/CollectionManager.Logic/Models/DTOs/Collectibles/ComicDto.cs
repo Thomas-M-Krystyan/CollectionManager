f642286683ced0d12e529bfc60e19e47d2a95420
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-12 00:18:40 +0200

    Update and rename ComicDto

---
+++ CollectionManager/Core/Application/CollectionManager.Logic/Models/DTOs/Collectibles/ComicDto.cs
@@ -0,0 +1,25 @@
+﻿using CollectionManager.Domain.Enums.Categories;
+using CollectionManager.Domain.Enums.FileExtensions;
+using CollectionManager.Domain.Enums.Genres;
+
+namespace CollectionManager.Logic.Models.DTOs.Collectibles
+{
+    public readonly struct ComicDto
+    {
+        public string Name { get; init; }
+
+        public string Authors { get; init; }
+
+        public Literatures Genre { get; init; }
+
+        public Ages Age { get; init; }
+
+        public DateOnly Published { get; init; }
+
+        public bool IsOwned { get; init; }
+
+        public string Notes { get; init; }
+
+        public FileDto<Graphics> Image { get; init; }
+    }
+}
