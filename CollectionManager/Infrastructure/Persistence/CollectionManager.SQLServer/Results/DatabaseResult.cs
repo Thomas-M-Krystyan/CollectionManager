dcf487b0d25d95c661900c3ca11759ac64b54285
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-12 15:25:36 +0200

    Rename generic parameter

--- CollectionManager/Infrastructure/Persistence/CollectionManager.SQLServer/Results/DatabaseResult.cs
+++ CollectionManager/Infrastructure/Persistence/CollectionManager.SQLServer/Results/DatabaseResult.cs
@@ -52,39 +52,39 @@ namespace CollectionManager.SQLServer.Results
     /// <summary>
     /// <inheritdoc cref="IDatabaseResult"/>
     /// <para>
-    ///   Containing a resulting entity <typeparamref name="TEntity"/>.
+    ///   Containing a <typeparamref name="TResult"/> result of the database query.
     /// </para>
     /// </summary>
-    /// <typeparam name="TEntity">The type of the entity.</typeparam>
-    public readonly struct DatabaseResult<TEntity> : IDatabaseResult
-        where TEntity : class
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    public readonly struct DatabaseResult<TResult> : IDatabaseResult
+        where TResult : class
     {
         /// <inheritdoc cref="IDatabaseResult.IsSuccess"/>
         public bool IsSuccess { get; }
 
         /// <inheritdoc cref="IDatabaseResult.IsFailure"/>
-        [MemberNotNullWhen(false, nameof(Entity))]
+        [MemberNotNullWhen(false, nameof(Result))]
         public bool IsFailure => !this.IsSuccess;
 
         /// <inheritdoc cref="IDatabaseResult.ChangesCount"/>
         public int ChangesCount { get; }
 
         /// <summary>
-        /// The entity attached to the result.
+        /// The result attached to the response.
         /// </summary>
-        public TEntity? Entity { get; }
+        public TResult? Result { get; }
 
         /// <inheritdoc cref="IDatabaseResult.Message"/>
         public string Message { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DatabaseResult{TEntity}"/> struct.
+        /// Initializes a new instance of the <see cref="DatabaseResult{TResult}"/> struct.
         /// </summary>
-        internal DatabaseResult(bool isSuccess, int changesCount, TEntity? entity, string message)
+        internal DatabaseResult(bool isSuccess, int changesCount, TResult? result, string message)
         {
             this.IsSuccess = isSuccess;
             this.ChangesCount = changesCount;
-            this.Entity = entity;
+            this.Result = result;
             this.Message = message;
         }
 
@@ -92,19 +92,19 @@ namespace CollectionManager.SQLServer.Results
         /// <summary>
         /// The positive outcome of the database operation.
         /// </summary>
-        internal static DatabaseResult<TEntity> Success(TEntity entity, string message)
-            => new(true, 0, entity, message);
+        internal static DatabaseResult<TResult> Success(TResult result, string message)
+            => new(true, 0, result, message);
 
         /// <summary>
         /// The positive outcome of the database operation.
         /// </summary>
-        internal static DatabaseResult<TEntity> Success(int changesCount, TEntity entity, string message)
-            => new(true, changesCount, entity, message);
+        internal static DatabaseResult<TResult> Success(int changesCount, TResult result, string message)
+            => new(true, changesCount, result, message);
 
         /// <summary>
         /// The negative outcome of the database operation.
         /// </summary>
-        internal static DatabaseResult<TEntity> Failure(string message)
+        internal static DatabaseResult<TResult> Failure(string message)
             => new(false, 0, null, message);
         #endregion
     }
