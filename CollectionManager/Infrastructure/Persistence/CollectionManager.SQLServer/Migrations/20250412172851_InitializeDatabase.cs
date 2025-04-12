aa1c89601c57f25f45dad8a79fa197a01d364933
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-12 19:56:46 +0200

    Add new migrations

---
+++ CollectionManager/Infrastructure/Persistence/CollectionManager.SQLServer/Migrations/20250412172851_InitializeDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollectionManager.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comics",
+                columns: table => new
+                {
+                    Id = table.Column<decimal>(type: "decimal(20,0)", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Series = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Volume = table.Column<byte>(type: "tinyint", nullable: false),
+                    Issues = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    Published = table.Column<DateOnly>(type: "date", nullable: false),
+                    IsOwned = table.Column<bool>(type: "bit", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comics");
+        }
+    }
+}
