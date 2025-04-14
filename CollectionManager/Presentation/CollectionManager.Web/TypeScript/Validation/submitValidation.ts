db35d7d4768472abdae6e4bad08e0fd0ca50d97e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2025-04-14 02:15:25 +0200

    Update TS submitValidation script

--- CollectionManager/Presentation/CollectionManager.Web/TypeScript/Validation/submitValidation.ts
+++ CollectionManager/Presentation/CollectionManager.Web/TypeScript/Validation/submitValidation.ts
@@ -2,27 +2,40 @@
 const messageEl = document.getElementById('number-check-message') as HTMLElement;
 const submitButton = document.querySelector('form input[type="submit"]') as HTMLInputElement;
 
+const emptyAnswer: string = '';
 const notNumber: string = 'ERROR: Not a number';
 const correctAnswer: string = 'Correct answer';
 const incorrectAnswer: string = 'Incorrect answer';
 
 inputEl.addEventListener('keyup', function () {
+    // Empty input
+    if (inputEl.value === emptyAnswer) {
+        messageEl.innerText = emptyAnswer;
+        submitButton.disabled = true;
+
+        return;
+    }
+
+    // Valid input
     const inputText: number = parseInt(inputEl.value);
 
-    let message: string = '';
-    let disable: boolean = true;
+    if (inputText % 2 === 0) {
+        messageEl.innerText = correctAnswer;
+        submitButton.disabled = false;
+
+        return;
+    }
+
+    // Invalid input
+    let message: string;
 
     if (isNaN(inputText)) {
         message = notNumber;
-    }
-    else if (inputText % 2 === 0) {
-        message = correctAnswer;
-        disable = false;
     }
     else {
         message = incorrectAnswer;
     }
 
     messageEl.innerText = message;
-    submitButton.disabled = disable;
+    submitButton.disabled = true;
 });
--- CollectionManager/Presentation/CollectionManager.Web/wwwroot/js/submitValidation.js
+++ CollectionManager/Presentation/CollectionManager.Web/wwwroot/js/submitValidation.js
@@ -1,24 +1,33 @@
 var inputEl = document.getElementById('number-check');
 var messageEl = document.getElementById('number-check-message');
 var submitButton = document.querySelector('form input[type="submit"]');
+var emptyAnswer = '';
 var notNumber = 'ERROR: Not a number';
 var correctAnswer = 'Correct answer';
 var incorrectAnswer = 'Incorrect answer';
 inputEl.addEventListener('keyup', function () {
+    // Empty input
+    if (inputEl.value === emptyAnswer) {
+        messageEl.innerText = emptyAnswer;
+        submitButton.disabled = true;
+        return;
+    }
+    // Valid input
     var inputText = parseInt(inputEl.value);
-    var message = '';
-    var disable = true;
+    if (inputText % 2 === 0) {
+        messageEl.innerText = correctAnswer;
+        submitButton.disabled = false;
+        return;
+    }
+    // Invalid input
+    var message;
     if (isNaN(inputText)) {
         message = notNumber;
-    }
-    else if (inputText % 2 === 0) {
-        message = correctAnswer;
-        disable = false;
     }
     else {
         message = incorrectAnswer;
     }
     messageEl.innerText = message;
-    submitButton.disabled = disable;
+    submitButton.disabled = true;
 });
 //# sourceMappingURL=submitValidation.js.map
